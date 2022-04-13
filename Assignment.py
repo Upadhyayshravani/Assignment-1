880b890dbdb3e615335a4e08724907ced670f0c7
Upadhyayshravani <93428861+Upadhyayshravani@users.noreply.github.com>
2022-04-13 23:44:56 +0530

    Create Assignment.py

---
+++ Assignment.py
@@ -0,0 +1,15 @@
+AREA OF CIRCLE- 
+
+Radius=int(input("ENTER THE RADIUS :"))
+Area_of_Circle= 3.14*Radius*Radius;
+print("AREA OF CIRCLE IS :",Area_of_Circle)
+
+INPUT OF STRING VALUE-
+
+file_name=str(input("Enter the file name "))
+if file_name==".py":
+    print("python")
+elif file_name==".c":
+    print("C program")
+else:
+    print("C++ Program")
