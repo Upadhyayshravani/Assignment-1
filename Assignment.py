f16bab816806ce98d3a548d6facc17280f81afe0
Upadhyayshravani <93428861+Upadhyayshravani@users.noreply.github.com>
2022-04-15 16:31:42 +0530

    Update Assignment.py

--- Assignment.py
+++ Assignment.py
@@ -4,12 +4,8 @@ Radius=int(input("ENTER THE RADIUS :"))
 Area_of_Circle= 3.14*Radius*Radius;
 print("AREA OF CIRCLE IS :",Area_of_Circle)
 
-INPUT OF STRING VALUE-
+Extact Extension name-
 
-file_name=str(input("Enter the file name "))
-if file_name==".py":
-    print("python")
-elif file_name==".c":
-    print("C program")
-else:
-    print("C++ Program")
+Filename= input("Enter Filename: ")
+E = Filename.split(".")
+print ("Extension Used is : " + E[-1])
